e34002a69b61d12fef5239a2ab69d61e21676355
Cloy Monis <cloy.m@vmax.com>
2022-02-01 19:00:39 +0530

    testing web hook

--- NGCorePocLib/Classes/Core/NGCorePocMain.swift
+++ NGCorePocLib/Classes/Core/NGCorePocMain.swift
@@ -9,7 +9,7 @@ import Foundation
 public class NGCorePocMain{
     
     public static let version = "0.0.14"
-    public var vmax = "b"
+    public var vmax = "d"
     
     public static func compute(lhs: Int, rhs:Int) -> Int{
         return lhs + rhs
