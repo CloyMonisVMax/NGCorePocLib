67cc09a960756c94b0fd2e240708418ff266d034
Cloy Monis <cloy.m@vmax.com>
2022-02-02 18:56:18 +0530

    source changes

--- NGCorePocLib/Classes/Core/NGCorePocMain.swift
+++ NGCorePocLib/Classes/Core/NGCorePocMain.swift
@@ -9,7 +9,7 @@ import Foundation
 public class NGCorePocMain{
     
     public static let version = "0.0.14"
-    public var vmax = "v"
+    public var vmax = "vservv"
     public var min = 23111
     
     public static func compute(lhs: Int, rhs:Int) -> Int{
